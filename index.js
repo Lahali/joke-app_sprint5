31e6e2411a6cc7b765d18c7a88a979962997241c
Halimeh Habib Diñeiro <halimehhabibdineiro@MacBook-Pro-de-Halimeh.local>
2022-07-14 09:23:06 +0200

    Ejercicio 2 solucionado. Se imprime el chiste en el navegador y funciona la recarga de chistes. No está maquetado

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
-const API_URL = "https://icanhazdadjoke.com/"
-
+const API_URL = "https://icanhazdadjoke.com/";
+const HTMLelement = document.getElementById("answer");
 
 async function getAJoke() {
-    const answer = await fetch(API_URL, {
-        headers: {
-            'Accept': 'application/json'
-        }
-    }).then((response) => response.json())
-    
-   console.log(answer) 
+  const answer = await fetch(API_URL, {
+    headers: {
+      Accept: "application/json",
+    },
+  }).then((response) => response.json())
+
+  console.log(answer);
+
+
+
+ HTMLelement.innerHTML = answer.joke
+
 }
 
-
+// fetch(`${API_URL}/users`)
+//   .then((response) => response.json())
+//   .then((users) => {
+//     const template = users.map((user) => `<li>${user.name} ${user.email}</li>`);
+//     HTMLResponse.innerHTML = `<ul>${template}</ul>`
+//   });
